afaeb002a519d3bdfcc1a15a5ea883f232ea366a
HobotM <mateuszhobot@gmail.com>
2022-07-28 23:40:38 +0100

    added name and time of tweet to the tweets list

--- pages/home.js
+++ pages/home.js
@@ -18,6 +18,9 @@ export default function Home( {tweets} ) {
     router.push("/");
   }
 
+  if (session && !session.user.name){
+    router.push('/setup')
+  }
   return (
     <>
       <NewTweet />
